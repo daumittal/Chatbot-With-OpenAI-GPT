0019b0e1d9ced0e4e5124abf575106d414b21851
Dau Mittal <daumittal@gmail.com>
2023-09-18 11:12:49 -0400

    Chatbot-With-OpenAI-GPT

--- website/routes.py
+++ website/routes.py
@@ -1,4 +1,14 @@
+from flask import Blueprint, render_template
+from flask import request
+from .models import Result
+import openai
+
+routes = Blueprint('routes', __name__)
+openai.api_key = 'YOUR_API_KEY_HERE'
+completion = openai.Completion()
+
 historyData = []
+
 @routes.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
@@ -16,3 +26,14 @@ def home():
         return render_template('response_view.html', results=dataList)
     else:
         return render_template('history.html', results=historyData)
+
+
+
+def ask(question, chat_log=None):
+    prompt = f'{chat_log}Human: {question}\nAI:'
+    response = completion.create(
+        prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
+        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
+        max_tokens=150)
+    answer = response.choices[0].text.strip()
+    return answer
